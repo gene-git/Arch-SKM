672bd20d979329c10cc4a4b311747162b8aef2f6
Gene C <arch@sapience.com>
2023-10-04 10:46:52 -0400

    Fix from Author: Expertcoderz <expertcoderzx@gmail.com>

--- Docs/conf.py
+++ Docs/conf.py
@@ -9,7 +9,7 @@
 project = "kernel-sign"
 copyright = '2023, Gene C'
 author = 'Gene C'
-release = '2.6.0'
+release = '2.6.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
