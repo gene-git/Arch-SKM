8ea78c34b5cd8ff2698262ed3d17191bb3bf7288
Gene C <arch@sapience.com>
2025-06-22 12:39:11 -0400

    run_prog: sync with latest from pyconcurrent

--- certs-local/lib/run_prog.py
+++ certs-local/lib/run_prog.py
@@ -4,7 +4,6 @@
 External program execution
 """
 # pylint: disable=too-many-arguments, too-many-positional-arguments
-# pylint: disable=too-many-locals, too-many-branches, too-many-statements
 # pylint: disable=consider-using-with
 from typing import (IO)
 import os
@@ -13,6 +12,82 @@ import io
 from select import select
 import subprocess
 from subprocess import SubprocessError
+
+
+class _ProcWaitInfo:
+    """
+    Little Data class used while waiting process to complete
+    """
+    # pylint: disable=too-many-instance-attributes
+    def __init__(self, proc: subprocess.Popen, bstring: bytes | None):
+        self.proc: subprocess.Popen = proc
+        self.bstring: bytes | None = bstring
+
+        self.has_stdout: bool = True
+        self.has_stderr: bool = True
+        self.has_stdin: bool = bool(bstring)
+        self.data_pending: bool = True
+
+        self.readlist: list[int | IO] = []
+        self.writelist: list[int | IO] = []
+        self.exceplist: list[int | IO] = []
+
+        # init non-blocking
+        self.nonblock()
+        self.is_data_pending()
+
+    def is_data_pending(self):
+        """
+        Any more data
+        """
+        self.data_pending = bool(self.has_stdout
+                                 or self.has_stderr
+                                 or self.has_stdin)
+
+    def nonblock(self):
+        """
+        Mark IO channels non-blocking
+        """
+        proc = self.proc
+        try:
+            if proc.stdin:
+                fcntl.fcntl(proc.stdin, fcntl.F_SETFL, os.O_NONBLOCK)
+
+            if proc.stdout:
+                fcntl.fcntl(proc.stdout, fcntl.F_SETFL, os.O_NONBLOCK)
+
+            if proc.stderr:
+                fcntl.fcntl(proc.stderr, fcntl.F_SETFL, os.O_NONBLOCK)
+
+        except OSError as err:
+            # Should not happen. Cross fingers and keep going.
+            print(f'Error setting NONBLOCK: {err}')
+
+    def update_select_iolists(self):
+        """
+        Update read/write lists for select
+        """
+        #
+        # Subprocess Read list
+        #
+        self.readlist = []
+        if self.has_stdout and self.proc.stdout:
+            self.readlist.append(self.proc.stdout)
+
+        if self.has_stderr and self.proc.stderr:
+            self.readlist.append(self.proc.stderr)
+
+        #
+        # Subprocess Write list
+        #
+        self.writelist = []
+        if self.has_stdin and self.proc.stdin:
+            self.writelist = [self.proc.stdin]
+
+        if not (self.readlist or self.writelist or self.exceplist):
+            self.data_pending = False
+
+        # No subprocess exception list
 
 
 def run_prog(pargs: list[str],
@@ -99,12 +174,7 @@ def run_prog(pargs: list[str],
 
     #
     # Wait for it to complete
-    # - we handle large output buffers by using non-blocking IO
-    #   and select() along with reading buffer/pipe to ensure
-    #   it never fills up and blocks.
-    #   Without this, larger output can hang hwne IO buffer is full.
     #
-
     (retc, output, errors) = _wait_for_proc(bstring, proc)
 
     return (retc, output, errors)
@@ -118,6 +188,11 @@ def _popen_proc(pargs: list[str],
                 ) -> tuple[bool, subprocess.Popen | None, str]:
     """
     Popen the process to run
+
+    Handle large output buffers using non-blocking IO
+    together with select() and reading of buffers/pipes to ensure
+    they never fill up and block.
+    Without this, larger output can hang when IO buffer is full.
 
     Args:
         See run_prog()
@@ -146,6 +221,7 @@ def _wait_for_proc(bstring: bytes | None,
                    proc: subprocess.Popen | None) -> tuple[int, str, str]:
     """
     Process has been opened, wait for process to complete.
+    Read/Write any data as may be needed.
 
     Args:
         proc (subprocess.Popen):
@@ -156,93 +232,108 @@ def _wait_for_proc(bstring: bytes | None,
     output = ''
     errors = ''
     timeout = 30
-    bufsiz = io.DEFAULT_BUFFER_SIZE
 
     if not proc:
         return (-1, '', 'process not started by popen - giving up')
 
+    pwi = _ProcWaitInfo(proc, bstring)
     returncode = proc.returncode
-    has_stdout = True
-    has_stderr = True
-    has_stdin = bool(bstring)
+    all_done: bool = False
 
-    try:
-        if proc.stdin:
-            fcntl.fcntl(proc.stdin, fcntl.F_SETFL, os.O_NONBLOCK)
-
-        if proc.stdout:
-            fcntl.fcntl(proc.stdout, fcntl.F_SETFL, os.O_NONBLOCK)
-
-        if proc.stderr:
-            fcntl.fcntl(proc.stderr, fcntl.F_SETFL, os.O_NONBLOCK)
-
-    except OSError as err:
-        # Should not happen. Cross fingers and keep going.
-        print(f'Error setting NONBLOCK: {err}')
-
-    data_pending = bool(has_stdout or has_stderr or has_stdin)
-
-    while returncode is None or data_pending:
+    while not all_done and (returncode is None or pwi.data_pending):
         returncode = proc.poll()
+        pwi.update_select_iolists()
 
         if returncode:
+            #
+            # Process exited.
+            # Allow final data check for possible unread buffered data.
+            #
+            all_done = True
+            break
+
+        if not pwi.data_pending:
             continue
 
-        readlist: list[int | IO] = []
-        if has_stdout and proc.stdout:
-            readlist.append(proc.stdout)
+        #
+        # Handle data
+        # Note if all_done ignore fail as just checking.
+        #
+        (okay, this_output, this_errors) = _check_for_data(pwi, timeout)
+        output += this_output
+        errors += this_errors
+        pwi.is_data_pending()
 
-        if has_stderr and proc.stderr:
-            readlist.append(proc.stderr)
-
-        writelist: list[int | IO] = []
-        if has_stdin and proc.stdin:
-            writelist = [proc.stdin]
-
-        exceplist: list[int | IO] = []
-
-        if not (readlist or writelist or exceplist):
-            data_pending = False
-            continue
-
-        try:
-            ready = select(readlist, writelist, exceplist, timeout)
-            read_ready = ready[0]
-            write_ready = ready[1]
-
-            try:
-                if has_stdin and proc.stdin and proc.stdin in write_ready:
-                    proc.stdin.write(bstring)
-                    proc.stdin.flush()
-                    proc.stdin.close()
-                    has_stdin = False
-
-                if has_stdout and proc.stdout and proc.stdout in read_ready:
-                    data = proc.stdout.read(bufsiz)
-                    if len(data) == 0:
-                        has_stdout = False
-                        proc.stdout.close()
-                    else:
-                        output += str(data, 'utf-8', errors='ignore')
-
-                if has_stderr and proc.stderr and proc.stderr in read_ready:
-                    data = proc.stderr.read(bufsiz)
-                    if len(data) == 0:
-                        has_stderr = False
-                        proc.stderr.close()
-                    else:
-                        errors += str(data, 'utf-8', errors='ignore')
-
-                data_pending = bool(has_stdout or has_stderr or has_stdin)
-
-            except (OSError, IOError, EOFError, ValueError) as err:
-                # read/write
-                return (-1, output, str(err) + ':\n' + errors)
-
-        except OSError as err:
-            # select
-            return (-1, output, str(err) + ':\n' + errors)
+        if not all_done and not okay:
+            return (-1, output, errors)
 
     # all done.
     retc = proc.returncode
+    if retc is None:
+        # should never happen
+        retc = -1
+
     return (retc, output, errors)
+
+
+def _check_for_data(pwi: _ProcWaitInfo, timeout: int) -> tuple[bool, str, str]:
+    """
+    Check if have any data.
+
+    Args:
+        pwi (_ProcWaitInfo):
+            The info needed to wait on any data
+
+        timeout (int):
+            timeout seconds for select()
+
+    Returns:
+        tuple[success: bool, stdout: str, stderr: str]
+    """
+    bufsiz = io.DEFAULT_BUFFER_SIZE
+
+    proc: subprocess.Popen = pwi.proc
+    bstring: bytes | None = pwi.bstring
+
+    output: str = ''
+    errors: str = ''
+
+    try:
+        ready = select(pwi.readlist, pwi.writelist, pwi.exceplist, timeout)
+        read_ready = ready[0]
+        write_ready = ready[1]
+
+        try:
+            if pwi.has_stdin and proc.stdin and proc.stdin in write_ready:
+                proc.stdin.write(bstring)
+                proc.stdin.flush()
+                proc.stdin.close()
+                pwi.has_stdin = False
+
+            if pwi.has_stdout and proc.stdout and proc.stdout in read_ready:
+                data = proc.stdout.read(bufsiz)
+                if len(data) == 0:
+                    pwi.has_stdout = False
+                    proc.stdout.close()
+                else:
+                    output += str(data, 'utf-8', errors='ignore')
+
+            if pwi.has_stderr and proc.stderr and proc.stderr in read_ready:
+                data = proc.stderr.read(bufsiz)
+                if len(data) == 0:
+                    pwi.has_stderr = False
+                    proc.stderr.close()
+                else:
+                    errors += str(data, 'utf-8', errors='ignore')
+
+            pwi.is_data_pending()
+
+        except (OSError, IOError, EOFError, ValueError) as err:
+            # read/write
+            return (False, output, str(err) + ':\n' + errors)
+
+    except (OSError, ValueError) as err:
+        # select
+        return (False, output, str(err) + ':\n' + errors)
+
+    return (True, output, errors)
